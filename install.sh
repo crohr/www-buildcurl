8e0df77fcb998f70339bbaefbfc311fcdb59755f
Cyril Rohr <cyril.rohr@gmail.com>
2016-02-09 18:01:04 +0000

    push

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+locale-gen en_US.UTF-8
+cat > /etc/default/locale <<EOF
+LC_ALL=en_US.UTF-8
+LANG=en_US.UTF-8
+EOF
+
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
 apt-get update -qq
