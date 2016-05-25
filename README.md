6dfd51d95d1d5cc0ddc0b718c9d112191c6999c1
Cyril Rohr <cyril.rohr@gmail.com>
2016-05-25 11:32:24 +0000

    update install

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Statically compiled software, built on demand
 Do you need ruby-2.3.0 for debian-8, to be installed into `/usr/local`? No problem:
 
 ```
-$ curl buildcurl.com \
+$ curl -GL buildcurl.com \
   -d recipe=ruby \
   -d version=2.3.0 \
   -d target=debian:8 \
@@ -16,7 +16,7 @@ $ curl buildcurl.com \
 ```
 
 ```
-$ ruby -v
+$ /usr/local/bin/ruby -v
 ruby 2.3.0p0 (2015-12-25 revision 53290) [x86_64-linux]
 ```
 
@@ -24,12 +24,20 @@ Perfect for Docker containers, or embedding binaries into packages, etc. It
 will take some time if this is the first time a binary is built, but then it
 will be cached.
 
+See USAGE.md for more details.
+
 ## Install
 
-docker run -d --name buildcurl -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock \
-  -e AWS_ACCESS_KEY_ID=key \
-  -e AWS_SECRET_ACCESS_KEY=secret \
-  -e AWS_REGION=us-east-1 \
-  -e AWS_BUCKET=cache.example.com \
-  -e BUILDCURL_URL=http://example.com:80 \
-  buildcurl/buildcurl
+From scratch, on a bare server (this also sets up init scripts, etc.):
+
+    NEWRELIC_KEY=optional AWS_ACCESS_KEY_ID=key AWS_SECRET_ACCESS_KEY=secret AWS_REGION=region AWS_BUCKET=bucket ./install.sh
+
+Or, assuming you already have docker installed and just want to test buildcurl:
+
+    docker run -d --name buildcurl -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock \
+      -e AWS_ACCESS_KEY_ID=key \
+      -e AWS_SECRET_ACCESS_KEY=secret \
+      -e AWS_REGION=us-east-1 \
+      -e AWS_BUCKET=cache.example.com \
+      -e BUILDCURL_URL=http://example.com:80 \
+      buildcurl/buildcurl
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
 locale-gen en_US.UTF-8
 cat > /etc/default/locale <<EOF
@@ -10,34 +11,23 @@ EOF
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
-
 echo deb http://apt.newrelic.com/debian/ newrelic non-free > /etc/apt/sources.list.d/newrelic.list
 wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
 apt-get update -qq
-apt-get install -y ruby apache2 curl linux-image-extra-$(uname -r) git htop newrelic-sysmond
+apt-get install -y curl linux-image-extra-$(uname -r) git htop newrelic-sysmond
 apt-get install -y docker-engine
-a2enmod cgi
-usermod -aG docker www-data
-gem install aws-sdk dotenv --no-ri --no-rdoc
 
-mkdir -p /opt
-test -d /opt/buildcurl || git clone https://github.com/crohr/buildcurl.git /opt/buildcurl
-chown -R www-data:www-data /opt/buildcurl
-mkdir -p /opt/buildcurl/cache
-chmod 2777 /opt/buildcurl/cache
-
-test -f /opt/buildcurl/.env || cat > /opt/buildcurl/.env <<EOF
+mkdir -p /etc/buildcurl
+test -f /etc/buildcurl/.env || cat > /etc/buildcurl/.env <<EOF
 BUILDCURL_URL=http://$(curl -s ifconfig.co)
 $(env | grep "AWS_")
 EOF
-
-rm -f /etc/apache2/sites-enabled/*.conf
-
-cp /opt/buildcurl/conf/buildcurl.conf /etc/apache2/sites-enabled/
-
-service apache2 restart
 
 if [ "$NEWRELIC_KEY" != "" ]; then
 	nrsysmond-config --set license_key=$NEWRELIC_KEY
 	service newrelic-sysmond restart
 fi
+
+docker pull buildcurl/buildcurl
+docker run -a stdout --rm --entrypoint /bin/cat buildcurl/buildcurl init/buildcurl.conf > /etc/init/buildcurl.conf
+service buildcurl restart
