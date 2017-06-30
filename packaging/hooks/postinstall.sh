f512a4261596adc1cbde30816a9fb1e0b5c36071
Cyril Rohr <cyril.rohr@gmail.com>
2017-06-30 23:33:52 +0200

    enable mod_cgi

--- packaging/hooks/postinstall.sh
+++ packaging/hooks/postinstall.sh
@@ -5,8 +5,15 @@ if ! gem list -i dotenv -v 2.2.1 ; then
 	gem install dotenv -v 2.2.1 --no-ri --no-rdoc
 fi
 
-$APP_NAME config:set AUTOUPGRADE=true
+$APP_NAME config:get AUTOUPGRADE || $APP_NAME config:set AUTOUPGRADE=true
 
 a2dissite 000-default
+
 cp -f $APP_HOME/conf/buildcurl.conf /etc/apache2/sites-enabled/buildcurl.conf
-systemctl reload apache2
+
+if [ ! -f /etc/apache2/mods-enabled/cgid.load ]; then
+	a2enmode cgi
+	systemctl restart apache2
+else
+	systemctl reload apache2
+fi
