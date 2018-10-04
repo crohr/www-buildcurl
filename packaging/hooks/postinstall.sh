e80104a6cbe954e579a720f9efd311a8d02ac6b7
Cyril Rohr <cyril.rohr@gmail.com>
2018-10-04 09:15:27 +0200

    fix

--- packaging/hooks/postinstall.sh
+++ packaging/hooks/postinstall.sh
@@ -12,7 +12,7 @@ a2dissite 000-default
 cp -f $APP_HOME/conf/buildcurl.conf /etc/apache2/sites-enabled/buildcurl.conf
 
 if [ ! -f /etc/apache2/mods-enabled/cgid.load ]; then
-	a2enmode cgi
+	a2enmod cgi
 	systemctl restart apache2
 else
 	systemctl reload apache2
